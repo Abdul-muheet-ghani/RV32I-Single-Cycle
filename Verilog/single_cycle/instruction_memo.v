98e6fe57d2c8c0eaf0b58eba03f72a12db0dd2f0
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2021-08-29 10:27:14 -0700

    Create instruction_memo.v

---
+++ Verilog/single_cycle/instruction_memo.v
@@ -0,0 +1,16 @@
+module ram (
+    clk,addr,data_in,data_out,wr
+ );
+
+ input clk,wr;
+ input [31:0]addr,data_in;
+ output wire[31:0] data_out;
+ reg [31:0] mem[2^32-1:0];
+
+ always @(*) begin
+    mem[addr]<=data_in;
+ end
+   assign data_out=mem[addr];
+
+    
+endmodule
