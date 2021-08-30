7a7ae19bfe146eba7676c8c5d0361da0dd46b886
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2021-08-30 02:29:12 -0700

    Create register_file.v

---
+++ Verilog/single_cycle/register_file.v
@@ -0,0 +1,17 @@
+module reg_file (
+   clk,rs1,rs2,rd,write_back,op_rs1,op_rs2,wr
+ );
+
+ input clk,wr;
+ input [31:0]write_back;
+ input [4:0]rs1,rs2,rd;
+ output wire[31:0]op_rs1,op_rs2;
+ reg [31:0] reg_mem[32-1:0];
+
+ always @(*) begin
+    reg_mem[rd]<=write_back;
+ end
+   assign op_rs1=reg_mem[rs1];
+   assign op_rs2=reg_mem[rs2];
+endmodule
+
