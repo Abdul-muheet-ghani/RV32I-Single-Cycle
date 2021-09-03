2b483ee2829b60a5f616c2c021e13caa130cb98e
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2021-09-03 14:57:06 +0500

    Update imm.v

--- Verilog/single_cycle/imm.v
+++ Verilog/single_cycle/imm.v
@@ -3,7 +3,7 @@ module immediate (
  );
  input [31:0]instruction,pc;
  output reg[31:0]I,S,SB,UJ,U ;
- wire [31:0]az,ay ;
+ wire [31:0]az,ay,ax ;
 
 
    assign az[0] = 1'b0;
@@ -11,19 +11,25 @@ module immediate (
    assign az[10:5] = instruction[30:25];
    assign az[11] = instruction[7];
    assign az[12] = instruction[31];
+   assign az[31:13] = 20'b00000000000000000000;
 
    assign ay[0] = 1'b0;
    assign ay[10:1] = instruction[30:21];
    assign ay[11] = instruction[20];
    assign ay[19:12] = instruction[19:12];
    assign ay[20] = instruction[31];
+   assign ay[31:21] = 10'b0000000000;
+
+   assign ax[19:0] = instruction[31:12];
+   assign ax[31:20] = 0;
 
  always @* begin
     I[11:0] = instruction[31:20] ;
+    I[31:12] = 20'b00000000000000000000;
     S[4:0] = instruction[11:7] ;
     S[11:5] = instruction[31:25];
     SB = pc + az;
     UJ = pc + ay;
-    U = instruction;
+    U = ax<<12;
  end
 endmodule
