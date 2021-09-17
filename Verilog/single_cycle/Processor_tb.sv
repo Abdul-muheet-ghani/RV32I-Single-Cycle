abac34dd3684c926e4680876f65166aa764cfa52
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2021-09-17 06:20:50 +0500

    Update Processor_tb.sv

--- Verilog/single_cycle/Processor_tb.sv
+++ Verilog/single_cycle/Processor_tb.sv
@@ -8,7 +8,14 @@ reg [31:0]din ;
 reg [31:0]addre ;
 wire [14:0]dout ;
 
+/*
+reg clk=1,we;
+reg [31:0]din1,din ;
+reg [31:0]addre ;
+wire [31:0]dout ;
+*/
 as m0(clk,din,dout,we);
+//ram m0(clk,din1,din,dout,we);
 
 always  begin
     #5; clk = ~clk;
@@ -17,8 +24,18 @@ end
 initial begin
     $dumpfile("asdasd788.vcd");
     $dumpvars(0,read_tb);
+    we=1;                  //processor
+    #10;
+    we=0; #10;
 
-    we=1;#10;
-    we=0; 
+/*
+    we=1;                     //ram
+    #10;
+    we=0; din1 = 0; #10;
+    we=0; din1 = 4; #10;
+    we=0; din1 = 8; #10;
+    we=0; din1 = 0; #10;
+*/
 end
+
 endmodule
