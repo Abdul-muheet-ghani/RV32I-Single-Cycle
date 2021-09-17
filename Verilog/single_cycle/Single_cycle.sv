908fb063376c1046bc35d676cdc5e0161140a167
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2021-09-18 00:58:52 +0500

    Update Single_cycle.sv

--- Verilog/single_cycle/Single_cycle.sv
+++ Verilog/single_cycle/Single_cycle.sv
@@ -1,7 +1,3 @@
-/*
-Abdul Muheet Ghani
-*/
-
 `timescale 1ns/1ns
 module ram (
     clk,addre,din,dout,we
@@ -13,35 +9,37 @@ module ram (
     reg [11:0]add1 ;
 
     always @(*) begin
-       #10;
        add1 = addre[13:2];
     end
 
     reg [31:0] mem[1024-1:0] ;
 
     initial begin
-        $readmemh("coef.txt",mem);
+        $readmemh("coef.mem",mem);
     end
     
     always @(posedge clk) begin
-        dout <= mem[add];
+       if (we==1) begin
+          mem[add1]<=din;
+       end 
+       else begin
+          dout <= mem[add1];
+       end
+         
     end
 endmodule
 //----------------------------------------------------
-module adder (clk,a);
+module adder (clk,adr,a);
 
 
  reg [31:0]c=0;
- output reg [31:0]a;
+ input [31:0]adr;
+ output wire [31:0]a;
  input clk;
 
-    always @(posedge clk ) begin
-       a <= c;
-    end
+ assign a = adr + 4;
 
-    always @* begin
-       c <= a+4;
-    end
+    
 endmodule
 //-----------------------------------------------------
 module control (
@@ -220,136 +218,46 @@ module reg_adr (
 endmodule
 //----------------------------------------------------
 module reg_file (
-   rs1,rs2,rd,inp,ou1,ou2,clk
+   rs1,rs2,rd,inp,ou1,ou2,clk,reset
  );
 
- input clk;
+ input clk,reset;
  input [4:0]rs1,rs2,rd;
  input [31:0]inp;
  output reg[31:0]ou1,ou2;
- reg [31:0]x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30,x31 ;
+ integer i;
+ 
+ reg [31:0]  reg_file1[31:0];
 
- always @(posedge clk) begin
-   case (rd)
-      0 : x0 = 0;
-      1 : x1 = inp;
-      2 : x2 = inp;
-      3 : x3 = inp;
-      4 : x4 = inp;
-      5 : x5 = inp;
-      6 : x6 = inp;
-      7 : x7 = inp;
-      8 : x8 = inp;
-      9 : x9 = inp;
-      10 : x10 = inp;
-      11 : x11 = inp;
-      12 : x12 = inp;
-      13 : x13 = inp;
-      14 : x14 = inp;
-      15 : x15 = inp;
-      16 : x16 = inp;
-      17 : x17 = inp;
-      18 : x18 = inp;
-      19 : x19 = inp;
-      20 : x20 = inp;
-      21 : x21 = inp;
-      22 : x22 = inp;
-      23 : x23 = inp;
-      24 : x24 = inp;
-      25 : x25 = inp;
-      26 : x26 = inp;
-      27 : x27 = inp;
-      28 : x28 = inp;
-      29 : x29 = inp;
-      30 : x30 = inp;
-      31 : x31 = inp;  
-      default : x0 = 0; 
-   endcase
-  end
-  always @* begin
-   case (rs1)
-      0 : ou1 = 0;
-      1 : ou1 = x1;
-      2 : ou1 = x2;
-      3 : ou1 = x3;
-      4 : ou1 = x4;
-      5 : ou1 = x5;
-      6 : ou1 = x6;
-      7 : ou1 = x7;
-      8 : ou1 = x8;
-      9 : ou1 = x9;
-      10 : ou1 = x10;
-      11 : ou1 = x11;
-      12 : ou1 = x12;
-      13 : ou1 = x13;
-      14 : ou1 = x14;
-      15 : ou1 = x15;
-      16 : ou1 = x16;
-      17 : ou1 = x17;
-      18 : ou1 = x18;
-      19 : ou1 = x19;
-      20 : ou1 = x20;
-      21 : ou1 = x21;
-      22 : ou1 = x22;
-      23 : ou1 = x23;
-      24 : ou1 = x24;
-      25 : ou1 = x25;
-      26 : ou1 = x25;
-      27 : ou1 = x27;
-      28 : ou1 = x28;
-      29 : ou1 = x29;
-      30 : ou1 = x30;
-      31 : ou1 = x31;
-
-      default : ou1 = 0; 
-   endcase
- //end
- //always @* begin
-    
-   case (rs2)
-      0 : ou2 = 0;
-      1 : ou2 = x1;
-      2 : ou2 = x2;
-      3 : ou2 = x3;
-      4 : ou2 = x4;
-      5 : ou2 = x5;
-      6 : ou2 = x6;
-      7 : ou2= x7;
-      8 : ou2= x8;
-      9 : ou2= x9;
-      10 : ou2 = x10;
-      11 : ou2 = x11;
-      12 : ou2 = x12;
-      13 : ou2 = x13;
-      14 : ou2 = x14;
-      15 : ou2 = x15;
-      16 : ou2 = x16;
-      17 : ou2 = x17;
-      18 : ou2 = x18;
-      19 : ou2 = x19;
-      20 : ou2 = x20;
-      21 : ou2 = x21;
-      22 : ou2 = x22;
-      23 : ou2 = x23;
-      24 : ou2 = x24;
-      25 : ou2 = x25;
-      26 : ou2 = x25;
-      27 : ou2 = x27;
-      28 : ou2 = x28;
-      29 : ou2 = x29;
-      30 : ou2 = x30;
-      31 : ou2 = x31;
-
-      default : ou2 = 0; 
-   endcase
+ always @(posedge clk ) begin
+    if (reset == 1) begin
+      for (i = 0 ; i<=31 ; i++) begin
+         reg_file1[i]=0;
+      end 
+    end else begin
+      reg_file1[rd] = inp;
+    end
  end
+ always @(*) begin
+      if (rs1==0) begin
+         ou1=0;
+      end else begin
+         ou1 = reg_file1[rs1];
+      end 
+      if (rs2==0) begin
+         ou2=0;
+      end else begin
+         ou2 = reg_file1[rs2];
+      end
+    end
+
 endmodule
 //----------------------------------------------------
 module immediate (
    instruction,pc,I,S,SB,UJ,U
  );
- input [31:0]instruction,pc;
- output reg[31:0]I,S,SB,UJ,U ;
+ input signed [31:0]instruction,pc;
+ output wire signed [31:0]I,S,SB,UJ,U ;
  wire [31:0]az,ay,ax ;
 
 
@@ -372,16 +280,16 @@ module immediate (
    assign ax[19:0] = instruction[31:12];
    assign ax[31:20] = 0;
 
- always @(*)  begin
-    I[11:0] = instruction[31:20] ;
-    I[31:12] = 20'b00000000000000000000;
-    S[4:0] = instruction[11:7] ;
-    S[11:5] = instruction[31:25];
-    S[31:12] = 20'b00000000000000000000;
-    SB = pc + az;
-    UJ = pc + ay;
-    U = ax<<12;
- end
+// always @(*)  begin
+   assign I[11:0] = instruction[31:20] ;
+   assign I[31:12] = 20'b00000000000000000000;
+   assign S[4:0] = instruction[11:7] ;
+   assign S[11:5] = instruction[31:25];
+   assign S[31:12] = 20'b00000000000000000000;
+   assign SB = pc + az;
+   assign UJ = pc + ay;
+   assign U = ax<<12;
+// end
 endmodule
 //----------------------------------------------------
 module ALU (
@@ -533,15 +441,33 @@ module mux2_4 (
  end
    
 endmodule
-//---------------------------------------------------
-module as (
-    clk,data_in,data_out,we
- );
+
+module reg_is (
+   in,out,clk,we
+);
 
  input clk,we;
+ input logic [31:0] in;
+ output logic [31:0] out;
+
+ always @(posedge clk) begin
+   if (we==1) begin
+      out <= 0;
+   end else begin
+      out <= in;
+   end
+end
+   
+endmodule
+//---------------------------------------------------
+module as (
+    clk,data_in,data_out,we,reset
+ );
+
+ input logic clk,we,reset;
  input [31:0]data_in ;
  output [14:0]data_out ;
- wire [31:0]OUT_T,addr,a,b,I,S,SB,UJ,U,op1,op2,faltu,reg_1,reg_2,im,ALU_OUTPUT,write_adder,write_ba,rite_ba ;
+ wire [31:0]OUT_T,addr,adr,a,b,I,S,SB,UJ,U,op1,op2,faltu,reg_1,reg_2,im,ALU_OUTPUT,write_adder,write_ba,rite_ba ;
  wire [8:0]c ;
  wire [4:0]r1,r2,rf ;
  reg [31:0]ac,addr1=0 ;
@@ -549,11 +475,12 @@ module as (
 
  //output [31:0]op1,op2;
 
- adder m0(clk,a);
- mux2_4 m12(next_pc,a,UJ,OUT_T,I + reg_1,addr);
- ram m1(clk,addr,data_in,b,we);
- control m2(b,c);
- unit m3(c,b,data_out);
+ adder adder(clk,adr,a);
+ mux2_4 mux2_4(next_pc,a,UJ,OUT_T,I + reg_1,addr);
+ reg_is reg_is(addr,adr,clk,reset);
+ ram ram(clk,addr,data_in,b,we);
+ control control(b,c);
+ unit unit(c,b,data_out);
  reg reg_write,mem_to_reg,mem_write,branch,opb;
  reg [3:0]alu_op;
  wire [31:0]zero = 0 ;
@@ -569,20 +496,20 @@ module as (
     mem_to_reg = data_out[13];
     reg_write = data_out[14];
  end
-
- reg_adr ms(b,r1,r2,rf,clk);
- reg_file m4(r1,r2,rf,rite_ba,op1,op2,clk);
- immediate m5(b,a,I,S,SB,UJ,U);
- mux2_4 m6(opa,op1,a,OUT_T,zero,reg_1);
- mux2_4 m7(imm_sel,zero,U,I,S,im);
- mux1_2 m8(opb,op2,im,reg_2);
- ALU m9(alu_op,reg_1,reg_2,ALU_OUTPUT);
- data_mem m10(clk,ALU_OUTPUT,op2,mem_write,mem_to_reg,write_adder);
- mux1_2 m11(mem_to_reg,ALU_OUTPUT,write_adder,write_ba);
- mux1_2 m14(mem_write,write_ba,zero,rite_ba);
- mux1_2 m13(branch,a,SB,OUT_T);
+ reg_adr reg_adr(b,r1,r2,rf,clk);
+ reg_file reg_file(r1,r2,rf,rite_ba,op1,op2,clk,reset);
+ immediate immediate(b,adr,I,S,SB,UJ,U);
+ mux2_4 mux2_40(opa,op1,a,OUT_T,zero,reg_1);
+ mux2_4 mux2_42(imm_sel,zero,U,I,S,im);
+ mux1_2 mux1_20(opb,op2,im,reg_2);
+ ALU ALU(alu_op,reg_1,reg_2,ALU_OUTPUT);
+ data_mem data_mem(clk,ALU_OUTPUT,op2,mem_write,mem_to_reg,write_adder);
+ mux1_2 mux1_21(mem_to_reg,ALU_OUTPUT,write_adder,write_ba);
+ mux1_2 mux1_22(mem_write,write_ba,zero,rite_ba);
+ mux1_2 mux1_23(branch,a,SB,OUT_T);
 endmodule
 //--------------------------------------------------
+/*
 module asd (
    clk,data_in,data_out,we
 );
@@ -594,4 +521,4 @@ module asd (
    adder m89(clk,a);
    ram m78(clk,a,data_in,data_out,we);
 
-endmodule
+endmodule*/
