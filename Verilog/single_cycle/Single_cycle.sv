ed38aa013bc04232f156952eaec080b63c71ce46
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2021-09-19 00:15:54 +0500

    Update Single_cycle.sv

--- Verilog/single_cycle/Single_cycle.sv
+++ Verilog/single_cycle/Single_cycle.sv
@@ -260,39 +260,46 @@ module immediate (
    instruction,pc,I,S,SB,UJ,U
  );
  input signed [31:0]instruction,pc;
- output wire signed [31:0]I,S,SB,UJ,U ;
- wire [31:0]az,ay,ax ;
+ output reg signed [31:0]S,SB,UJ,U,I ;
+ reg signed [31:0]az,ay,ax ;
 
+   always @(*) begin 
+   az[0] = 1'b0;
+   az[4:1] = instruction[11:8];
+   az[10:5] = instruction[30:25];
+   az[11] = instruction[7];
+   az[12] = instruction[31];
+   if (instruction[31]==1) begin
+      az[31:13] = 20'b11111111111111111111;
+      ay[31:21] = 12'b111111111111;
+   end else begin
+      az[31:13] = 20'b00000000000000000000;
+      ay[31:21] = 12'b000000000000;
+   end
 
+   ay[0] = 1'b0;
+   ay[10:1] = instruction[30:21];
+   ay[11] = instruction[20];
+   ay[19:12] = instruction[19:12];
+   ay[20] = instruction[31];
 
-   assign az[0] = 1'b0;
-   assign az[4:1] = instruction[11:8];
-   assign az[10:5] = instruction[30:25];
-   assign az[11] = instruction[7];
-   assign az[12] = instruction[31];
-   assign az[31:13] = 20'b00000000000000000000;
+   ax[19:0] = instruction[31:12];
+   ax[31:20] = 0;
 
-   assign ay[0] = 1'b0;
-   assign ay[10:1] = instruction[30:21];
-   assign ay[11] = instruction[20];
-   assign ay[19:12] = instruction[19:12];
-   assign ay[20] = instruction[31];
-
-   assign ay[31:21] = 12'b000000000000;
-
-   assign ax[19:0] = instruction[31:12];
-   assign ax[31:20] = 0;
-
-// always @(*)  begin
-   assign I[11:0] = instruction[31:20] ;
-   assign I[31:12] = 20'b00000000000000000000;
-   assign S[4:0] = instruction[11:7] ;
-   assign S[11:5] = instruction[31:25];
-   assign S[31:12] = 20'b00000000000000000000;
-   assign SB = pc + az;
-   assign UJ = pc + ay;
-   assign U = ax<<12;
-// end
+   I[11:0] = instruction[31:20] ;
+   if (instruction[31]==1) begin
+      I[31:12] = 20'b11111111111111111111;
+      S[31:12] = 20'b11111111111111111111;
+   end else begin
+      I[31:12] = 20'b00000000000000000000;
+      S[31:12] = 20'b00000000000000000000;
+   end
+   S[4:0] = instruction[11:7] ;
+   S[11:5] = instruction[31:25];
+   SB = pc + az;
+   UJ = pc + ay;
+   U = ax<<12;
+   end
 endmodule
 //----------------------------------------------------
 module ALU (
