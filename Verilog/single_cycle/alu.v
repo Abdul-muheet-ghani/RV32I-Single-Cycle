3c054e707cfdfee9b6b2bafeabaf09a08464cbcb
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2021-09-27 21:30:19 +0500

    Update alu.v

--- Verilog/single_cycle/alu.v
+++ Verilog/single_cycle/alu.v
@@ -3,7 +3,7 @@ module ALU (
  );
    input [3:0]op;
    input [31:0]op1,op2;
-   output reg [31:0]res ;
+   output reg signed [31:0]res ;
 
 
  always @* begin
@@ -14,11 +14,11 @@ module ALU (
          4'b0011 : res = op1 | op2;
          4'b0100 : res = op1 ^ op2;
          4'b0101 : res = op1 << op2;
-         4'b0101 : res = op1 >> op2;
-         4'b0110 : res = op1 > op2;
-         4'b0111 : res = op1 > op2;
-         4'b1000 : res = op1 >> op2;
-         4'b1111 : res = op1 + op2;
+         4'b0110 : res = op1 >> op2;
+         4'b0111 : res[0] = (op1 < op2) ? 1 : 0;
+         4'b1000 : res[0] = (op1 < op2) ? 1 : 0;
+         4'b1001 : res = op1 >>> op2;
+         4'b1111 : res = op1 ;
          default: res = 0;
       endcase
  end
