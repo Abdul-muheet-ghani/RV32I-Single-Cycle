74b3b134e97230ce2eb73bcd103e73b92f8804d0
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2021-09-27 21:28:28 +0500

    Update imm.v

--- Verilog/single_cycle/imm.v
+++ Verilog/single_cycle/imm.v
@@ -1,36 +1,35 @@
 module immediate (
    instruction,pc,I,S,SB,UJ,U
  );
- input [31:0]instruction,pc;
- output reg[31:0]I,S,SB,UJ,U ;
- wire [31:0]az,ay,ax ;
+ input signed [31:0]instruction,pc;
+ output reg signed [31:0]S,SB,UJ,U,I ;
+ reg [11:0]II ,SS ,az;
+ reg [19:0]ay;
+ reg signed [31:0]ax ;
 
+   always @(*) begin 
+   az[0] = 1'b0;
+   az[4:1] = instruction[11:8];
+   az[10:5] = instruction[30:25];
+   az[11] = instruction[7];
+   az[12] = instruction[31];
 
-   assign az[0] = 1'b0;
-   assign az[4:1] = instruction[11:8];
-   assign az[10:5] = instruction[30:25];
-   assign az[11] = instruction[7];
-   assign az[12] = instruction[31];
-   assign az[31:13] = 20'b00000000000000000000;
+   ay[0] = 1'b0;
+   ay[10:1] = instruction[30:21];
+   ay[11] = instruction[20];
+   ay[19:12] = instruction[19:12];
+   ay[20] = instruction[31];
 
-   assign ay[0] = 1'b0;
-   assign ay[10:1] = instruction[30:21];
-   assign ay[11] = instruction[20];
-   assign ay[19:12] = instruction[19:12];
-   assign ay[20] = instruction[31];
-   assign ay[31:21] = 10'b0000000000;
+   ax[19:0] = instruction[31:12];
+   ax[31:20] = 0;
 
-   assign ax[19:0] = instruction[31:12];
-   assign ax[31:20] = 0;
-
- always @* begin
-    I[11:0] = instruction[31:20] ;
-    I[31:12] = 20'b00000000000000000000;
-    S[4:0] = instruction[11:7] ;
-    S[11:5] = instruction[31:25];
-    S[31:12] = 20'b00000000000000000000;
-    SB = pc + az;
-    UJ = pc + ay;
-    U = ax<<12;
- end
+   II = instruction[31:20];
+   I = {{20{II[11]}},II};
+   SS[4:0] = instruction[11:7] ;
+   SS[11:5] = instruction[31:25];
+   S = {{20{SS[11]}},SS};
+   SB = pc + {{19{az[12]}},az};
+   UJ = pc + {{11{ay[20]}},ay};
+   U = ax << 12;
+   end
 endmodule
