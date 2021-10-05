85b202ebefeb4d224bdfdb545865d83045120b77
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2021-10-05 12:15:46 +0500

    Create ALU.v

---
+++ RTL/ALU.v
@@ -0,0 +1,26 @@
+module ALU (
+   op,op1,op2,res
+ );
+   input [3:0]op;
+   input [31:0]op1,op2;
+   output reg signed [31:0]res ;
+
+
+ always @* begin
+      case (op)
+         4'b0000 : res = op1 + op2; 
+         4'b0001 : res = op1 - op2;
+         4'b0010 : res = op1 & op2;
+         4'b0011 : res = op1 | op2;
+         4'b0100 : res = op1 ^ op2;
+         4'b0101 : res = op1 << op2;
+         4'b0110 : res = op1 >> op2;
+         4'b0111 : res[0] = (op1 < op2) ? 1 : 0;
+         4'b1000 : res[0] = ($signed (op1) < $signed (op2)) ? 1 : 0;
+         4'b1001 : res = op1 >>> op2;
+         4'b1111 : res = op1 ;
+         default: res = 0;
+      endcase
+ end
+
+endmodule
