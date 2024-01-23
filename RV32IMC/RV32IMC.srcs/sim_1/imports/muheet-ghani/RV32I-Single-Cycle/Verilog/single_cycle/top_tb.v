d6222ba71fc6ba42f5c98e98a46acf8d19b9630d
Abdul-muheet-ghani <aghani@students.uit.edu>
2024-01-23 22:59:03 +0500

    Clean up of the top tb

---
+++ RV32IMC/RV32IMC.srcs/sim_1/imports/muheet-ghani/RV32I-Single-Cycle/Verilog/single_cycle/top_tb.v
@@ -0,0 +1,60 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: MERL-UITU
+// Engineer: Abdul Muheet Ghani
+// 
+// Design Name: RV32IMACZicsr for Linux
+// Module Name: top_tb
+// Project Name: RV32IMACZicsr for linux
+// Target Devices: 
+// Description: 
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/1ns
+
+module top_tb;
+
+    /////////////////////////////////////////////////////////
+    //ports
+    /////////////////////////////////////////////////////////
+
+    reg         clk=1,we,reset;
+    reg  [31:0] data_in;
+    wire [31:0] d1,d2;
+    wire [14:0] data_out;
+
+    /////////////////////////////////////////////////////////
+    //Module Instantiation
+    /////////////////////////////////////////////////////////
+
+    RV32I RV32I_i(.clk        (clk),
+                  .we         (we),
+                  .reset      (reset),
+                  .data_in    (data_in),
+                  .data_out   (data_out)
+    );
+
+    /////////////////////////////////////////////////////////
+    //always block
+    /////////////////////////////////////////////////////////
+
+    always begin
+        #5;
+        clk = ~clk;
+        reset = 0;
+    end
+    
+    /////////////////////////////////////////////////////////
+    //procedural assignment
+    /////////////////////////////////////////////////////////
+
+    initial begin
+        $dumpfile("fetch.vcd");
+        $dumpvars(0,memo_tb);
+        we = 1;
+        #10;
+        we = 0;
+        #1000;
+        $finish();
+    end
+    
+endmodule
